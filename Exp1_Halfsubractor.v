d7ff7cc34fb0a00090be607d3333894764f59259
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:27:21 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,8 @@
-module hs(a,b,difference,borrow);
-input a,b;
-output difference,borrow;
-
-
-
+module halfsub(x,y,d,b);
+input x,y;
+output d,b;
+wire x1;
+xor g1(d,x,y);
+not g2(x1,x);
+and g3(b,x1,y);
 endmodule
