2cbe3fb1c69f24153afbfdaa62d73d52fbf6cc57
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:29:45 +0530

    Update Exp1_Ripplecarryadder_4bit.v

--- Exp1_Ripplecarryadder_4bit.v
+++ Exp1_Ripplecarryadder_4bit.v
@@ -1,11 +1,10 @@
-module rca_4bit(a,b,c,s,carry);
-input [3:0]a,b;
-input c;
-output [3:0]s;
-output carry;
-
-
-
-
-
+module fulladder(X, Y, Ci, S, Co);
+input X, Y, Ci;
+output S, Co;
+wire w1,w2,w3;
+xor G1(w1, X, Y);
+xor G2(S, w1, Ci);
+and G3(w2, w1, Ci);
+and G4(w3, X, Y);
+or G5(Co, w2, w3);
 endmodule
